e681f93858a4296ddd7a8fc99e2d7e54144b8f52
ianseeingbeing <igillette2006@gmail.com>
2024-12-20 01:00:11 -0500

    ready for the server

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+    kit: {
+        // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+        // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+        // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+        adapter: adapter({
+            out: "build",
+            // pages: "build",
+            assets: "build",
+            fallback: "undefined",
+            precompress: false,
+            strict: true,
+        }),
+    },
 };
 
 export default config;
