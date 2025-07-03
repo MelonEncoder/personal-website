2c4cc18877e9aff7b810b3fb0000cb41c9eee45b
MelonEncoder <iangillette@proton.me>
2025-07-03 01:32:17 -0400

    jk, going back to svelte

---
+++ src/lib/mono.svelte
@@ -0,0 +1,51 @@
+<script>
+    import "../style.css";
+</script>
+
+<div id="monogram-layer-3">
+    <div id="monogram-layer-2">
+        <div id="monogram-layer-1">
+            <img id="monogram" src="monogram.svg" alt="ian's monogram" />
+        </div>
+    </div>
+</div>
+
+<style>
+    #monogram-layer-1 {
+        background-color: var(--red);
+        padding: 35px 10px;
+        margin: 0;
+        border-radius: 120px;
+        box-shadow: 0px 0px 10px 10px rgba(0, 0, 0, 0.2);
+    }
+
+    #monogram-layer-1:hover {
+        cursor: pointer;
+        border-width: 5px;
+        border-color: white;
+        border-color: blue;
+    }
+
+    #monogram-layer-1:active {
+        scale: 0.95;
+        box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
+        background-color: var(--dark-red);
+    }
+
+    #monogram-layer-2 {
+        background-color: var(--dark-red);
+        border-radius: 140px;
+        padding: 20px;
+        margin: 0;
+        box-shadow: 0px 0px 10px 10px rgba(0, 0, 0, 0.2);
+    }
+
+    #monogram-layer-3 {
+        background-color: var(--darker-red);
+        width: 280px;
+        border-radius: 180px;
+        padding: 20px;
+        margin: auto;
+        box-shadow: 0px 0px 10px 10px rgba(0, 0, 0, 0.2);
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+    import "../style.css";
+    import Mono from "$lib/mono.svelte";
+</script>
+
+<nav>
+    <ul>
+        <li>Home</li>
+        <li>Contact Me</li>
+        <li>Projects</li>
+    </ul>
+</nav>
+
+<div id="middle">
+    <div id="m-center">
+        <Mono></Mono>
+    </div>
+</div>
+
+<style>
+    :global(body) {
+        margin: 0;
+        background-color: var(--white);
+    }
+    #middle {
+        display: flex;
+        flex-direction: row;
+        height: 50vh;
+        margin: 25vh 0;
+        border-top: 2px black solid;
+        border-bottom: 2px black solid;
+    }
+    #m-center {
+        position: static;
+        width: 100vw;
+        background-color: var(--black);
+    }
+    nav {
+        font-family: var(--font-body);
+    }
+</style>
