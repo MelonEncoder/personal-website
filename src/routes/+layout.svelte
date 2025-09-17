a6f9d054d4280a80583e21c9ac3de25969490ef1
MelonEncoder <iangillette@proton.me>
2025-09-17 00:00:49 -0400

    added content to webpage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,3 +9,20 @@
 </svelte:head>
 
 {@render children?.()}
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Cascadia+Mono:ital,wght@0,200..700;1,200..700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+	:root {
+		--black-1: rgb(33 33 35);
+
+		--white-1: rgb(251 250 245);
+		--white-2: rgb(221 220 215);
+	}
+
+	:global(body) {
+		font-family: 'Cascadia Mono';
+		min-height: 100vh;
+		margin: 0;
+		background-color: var(--white-1);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,71 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import monogram from '$lib/assets/monogram.svg';
+
+	function aboutBtn() {}
+</script>
+
+<div id="page">
+	<div id="container">
+		<img src={monogram} alt="ian's monogram" width="100px" />
+		<h1>Ian Gillette</h1>
+		<nav>
+			<ul>
+				<li>
+					<button onclick={aboutBtn}> about </button>
+				</li>
+				<li>
+					<button>projects</button>
+				</li>
+				<li><button>graphic</button></li>
+			</ul>
+		</nav>
+	</div>
+</div>
+
+<style>
+	#page {
+		padding: 0 1rem;
+	}
+
+	#container {
+		display: flex;
+		flex-direction: column;
+		place-items: center;
+		justify-self: center;
+		align-self: center;
+		padding: 40px;
+		background-color: var(--black-1);
+		color: var(--white-1);
+		max-width: 700px;
+		margin: auto;
+		border-bottom-left-radius: 1rem;
+		border-bottom-right-radius: 1rem;
+	}
+
+	ul {
+		display: flex;
+		flex-direction: row;
+		padding: 0;
+		list-style: none;
+	}
+
+	button {
+		padding: 0.5rem 0.75rem;
+		border: 2px solid var(--white-1);
+		margin: 0px 0.5rem;
+		border-radius: 0.4rem;
+		background-color: transparent;
+		color: var(--white-1);
+		font-size: 1rem;
+	}
+
+	button:hover {
+		cursor: pointer;
+		background-color: var(--white-1);
+		color: var(--black-1);
+	}
+
+	button:active {
+		background-color: var(--white-2);
+	}
+</style>
